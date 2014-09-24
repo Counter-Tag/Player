820642c567936309403803c4b2653b8adcf364dc
Roobre <roobre@roobre.es>
2014-09-24 03:18:56 +0200

    Added file to hold pin definitions.

---
+++ include/common.h
@@ -0,0 +1,11 @@
+#ifndef PINS_H
+#define PINS_H
+
+const int DISPLAY_PIN_RS = 12;
+const int DISPLAY_PIN_E  = 11;
+const int DISPLAY_PIN_D4 = 16;
+const int DISPLAY_PIN_D5 = 17;
+const int DISPLAY_PIN_D6 = 18;
+const int DISPLAY_PIN_D7 = 19;
+
+#endif
