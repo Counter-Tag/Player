92e62fd1e6429db47dda30de3c147272a65976c3
Roberto "Roobre" Santalla <roobre@roobre.es>
2015-01-30 10:48:19 +0100

    Removed legacy factory functions.

--- include/Player/player_factory.h
+++
@@ -1,5 +0,0 @@
-#include "Player.h"
-
-Player* create_player(int id);
-
-Player* create_player(const char* name);
--- include/Weapon/weapon_factory.h
+++
@@ -1,6 +0,0 @@
-#include "Weapon.h"
-#include "AK47.h"
-
-Weapon* create_weapon(uint8_t id);
-
-Weapon* create_weapon(const char* name);
