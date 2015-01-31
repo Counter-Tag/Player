f3a9016fad7b392e0c57af9c6220f39de1f53ce8
Roberto "Roobre" Santalla <roobre@roobre.es>
2015-01-31 17:53:23 +0100

    Changed conflictive pins and added a few more.

--- include/common.h
+++ include/common.h
@@ -1,8 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-const int DISPLAY_PIN_RS = 12;
-const int DISPLAY_PIN_E  = 11;
+const int DISPLAY_PIN_E  = 14;
+const int DISPLAY_PIN_RS = 15;
 const int DISPLAY_PIN_D4 = 16;
 const int DISPLAY_PIN_D5 = 17;
 const int DISPLAY_PIN_D6 = 18;
@@ -11,6 +11,11 @@ const int DISPLAY_PIN_D7 = 19;
 const int RELOAD_PIN = 5;
 const int FIRE_PIN = 6;
 const int SKILL_PIN = 7;
+
+const int SDCS_PIN = 4;
+const int SPKR_PIN = 9; // Forced on non-mega boards due to timer constraints
+
+const int SPAWN_PIN = 12;
 
 const int INTR_PIN = 0; // Means pin 2
 const int RECV_PIN = 2;
