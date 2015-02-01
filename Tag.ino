09d2814fbd4998e478791dd4780b62ffe77fce9d
Roberto "Roobre" Santalla <roobre@roobre.es>
2015-02-01 23:55:39 +0100

    Added SD initialization.

--- Tag.ino
+++ Tag.ino
@@ -1,9 +1,11 @@
 #include "include/Tag.h"
 
 Tag tag;
+TMRpcm tmrpcm;
 
 void setup() {
     Serial.begin(9600);
+    SD.begin(SDCS_PIN);
 
     Serial.println("Spawning player...");
     tag.spawn("Trooper", "AK47", 0);
