4b28189558d1e8576e3be44d3afb4bb1fa2b8cdf
Roberto "Roobre" Santalla <roobre@roobre.es>
2015-02-01 14:04:26 +0100

    Cleaned up main ino file.

--- Tag.ino
+++ Tag.ino
@@ -1,21 +1,14 @@
 #include "include/Tag.h"
 
 Tag tag;
-shot_t enemyDmgShot;
-shot_t allyHealShot;
 
 void setup() {
-    enemyDmgShot = {10, WF_DAMAGE_ENEMIES | 0b00000100};
-    allyHealShot = {5, WF_DAMAGE_ENEMIES | WF_HEAL_ALLIES};
-
     Serial.begin(9600);
 
     Serial.println("Spawning player...");
-    tag.spawn("Trooper", "AK47");
+    tag.spawn("Trooper", "AK47", 0);
 }
 
 void loop() {
-    tag.fire();
-    //tag.receiveShot(&enemyDmgShot);
-    delay(250);
+    tag.loop();
 }
